e9544de828f45d32acba31d5a03d5f34b9160da9
Mher Saribekyan <saribekyanmher@gmail.com>
2025-11-11 19:50:08 +0400

    craps game

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # craps
-Craps game in python
+
+##Craps game in python
+
+The player should roll two dice. If the sum of both of them is 7 or 11 the player wins. If the sum is 2, 3 or 12 (craps) the casino wins. If during the first roll the sum is 4, 5, 6, 8, 9 or 10, that number becomes the “goal” number. To win, the player should roll the dice till they roll the goal number again. If the player rolls a 7 before rolling the goal number, they lose.
+
+### How to run
+
+Make sure you have python installed on your computer. Clone the repository and run
+
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+
+def roll_dice():
+    die1 = random.randint(1,6)
+    die2 = random.randint(1,6)
+    sum = die1+die2
+    print(f"""The sum of dice is {die1} + {die2} = {sum}""")
+    return sum
+
+def start_game():
+    # Stage 1
+    sum = roll_dice()
+
+    if sum == 7 or sum == 11:
+        print("You won")
+    elif sum == 2 or sum == 3 or sum == 12:
+        print("You lose")
+    else:
+        # Stage 2
+        target = sum
+        print(f"""Now your goal number is {target}""") 
+        while(True):
+            sum = roll_dice()
+            if sum == target:
+                print("You won")
+                break
+            elif sum == 7:
+                print("You lose")
+                break
+
+start_game()
